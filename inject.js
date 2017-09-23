62badc4b99d3fe0e4b43abc5d38d3eeb22dae77d
Eliran Pe'er <eliran.peer@forter.com>
2017-09-23 20:18:27 +0300

    Getting rid of jQuery

--- inject.js
+++ inject.js
@@ -2,35 +2,25 @@
 *	Entry
 */
 chrome.extension.sendMessage({}, function (response) {
-	var readyStateCheckInterval = setInterval(function () {
-		if (document.readyState === 'complete') {
-			clearInterval(readyStateCheckInterval);
+    var readyStateCheckInterval = setInterval(function () {
+        if (document.readyState === 'complete') {
+            clearInterval(readyStateCheckInterval);
 
-			console.log('Mac Reload is running');
-            
+            console.log('Mac Reload is running');
+
             AddDom();
-		}
-	}, 10);
+        }
+    }, 10);
 });
 
-let mostReventScrollEvent = 0;
-
 function AddDom() {
-    $('body').prepend(`
-        <div 
-            id="p2r" 
-            style="position: fixed; top: -20px; z-index: 99999; height: 15px; width: 100%; text-align: center;">
-                Release to Reload
-        </div>
-    `);
+    const elem = document.createElement('div');
+    elem.style = 'user-select: none; position: fixed; top: -20px; z-index: 99999; height: 15px; width: 100%; text-align: center;';
+    elem.id = 'p2r';
+    elem.innerText = 'Reloading...';
 
-    window.addEventListener('mousewheel', e => {
-        const timeOfEvent = new Date().getTime();
-        setTimeout(() => {
-            if ((timeOfEvent - 1000) > mostReventScrollEvent) {
-                console.log("Overscroll detected");
-            }
-        });
-    });
-    window.addEventListener('scroll', e => mostReventScrollEvent = new Date());
+    document.body.insertBefore(elem, document.body.firstChild);
+
+    let isReloading = false;
+    overscroll(e => e.direction === 'up' && !isReloading && (console.log('reloading...'), isReloading = true) && document.location.reload());
 }
