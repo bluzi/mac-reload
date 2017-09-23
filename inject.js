c0e0b5b22b27d65f12ed8513fc04e15bffb14fd6
Eliran Pe'er <eliran.peer@forter.com>
2017-09-23 15:33:14 +0300

    Add 'Release to Reload' message to the top of every webpage

--- inject.js
+++ inject.js
@@ -7,7 +7,18 @@ chrome.extension.sendMessage({}, function (response) {
 			clearInterval(readyStateCheckInterval);
 
 			console.log('Mac Reload is running');
-			
+            
+            AddDom();
 		}
 	}, 10);
 });
+
+function AddDom() {
+    $('body').prepend(`
+        <div 
+            id="p2r" 
+            style="position: fixed; top: -20px; z-index: 99999; height: 15px; width: 100%; text-align: center;">
+                Release to Reload
+        </div>
+    `);
+}
