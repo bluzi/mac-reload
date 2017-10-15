e28c259113655f0dd001a410e44e6cdd440b7f19
Alexandre Resende <aar6947@gmail.com>
2017-10-15 21:55:26 -0200

    refactor: Add arrow functions and changed some variables names

--- inject.js
+++ inject.js
@@ -1,7 +1,7 @@
 /*
 *	Entry
 */
-chrome.extension.sendMessage({}, function (response) {
+chrome.extension.sendMessage({}, (response) => {
     var readyStateCheckInterval = setInterval(function () {
         if (document.readyState === 'complete') {
             clearInterval(readyStateCheckInterval);
@@ -13,14 +13,19 @@ chrome.extension.sendMessage({}, function (response) {
     }, 10);
 });
 
-function AddDom() {
-    const elem = document.createElement('div');
-    elem.style = 'user-select: none; position: fixed; top: -20px; z-index: 99999; height: 15px; width: 100%; text-align: center;';
-    elem.id = 'p2r';
-    elem.innerText = 'Reloading...';
+const AddDom = () => {
 
-    document.body.insertBefore(elem, document.body.firstChild);
+    const divElement = document.createElement('div');
+
+    divElement.style = 'user-select: none; position: fixed; top: -20px; z-index: 99999; height: 15px; width: 100%; text-align: center;';
+    divElement.id = 'p2r';
+    divElement.innerText = 'Reloading...';
+
+    document.body.insertBefore(divElement, document.body.firstChild);
 
     let isReloading = false;
-    overscroll(e => e.direction === 'up' && !isReloading && (console.log('reloading...'), isReloading = true) && document.location.reload());
+    overscroll(
+        e => e.direction === 'up' && !isReloading && (console.log('reloading...'), isReloading = true) && document.location.reload()
+    );
+
 }
