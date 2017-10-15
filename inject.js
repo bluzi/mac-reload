02ad6637662f2dc07d09c68587a0e1e3aa2cf5ce
vanchagreen <vanchagreen@gmail.com>
2017-10-15 14:12:27 -0600

    #4: Improving code readability

--- inject.js
+++ inject.js
@@ -2,25 +2,37 @@
 *	Entry
 */
 chrome.extension.sendMessage({}, function (response) {
-    var readyStateCheckInterval = setInterval(function () {
+    const intervalId = setInterval(() => {
         if (document.readyState === 'complete') {
-            clearInterval(readyStateCheckInterval);
-
+            clearInterval(intervalId);
             console.log('Mac Reload is running');
-
-            AddDom();
+            addDOM();
         }
     }, 10);
 });
 
-function AddDom() {
+function addDOM() {
     const elem = document.createElement('div');
-    elem.style = 'user-select: none; position: fixed; top: -20px; z-index: 99999; height: 15px; width: 100%; text-align: center;';
     elem.id = 'p2r';
     elem.innerText = 'Reloading...';
+    elem.style = `
+        user-select: none;
+        position: fixed;
+        top: -20px;
+        z-index: 99999;
+        height: 15px;
+        width: 100%;
+        text-align: center;
+    `;
 
     document.body.insertBefore(elem, document.body.firstChild);
 
     let isReloading = false;
-    overscroll(e => e.direction === 'up' && !isReloading && (console.log('reloading...'), isReloading = true) && document.location.reload());
+    overscroll(e => {
+        if (e.direction === 'up' && !isReloading) {
+            console.log('reloading...');
+            isReloading = true;
+            document.location.reload();
+        }
+    });
 }
