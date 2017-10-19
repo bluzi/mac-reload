411296aa9d451c4e4ff4d2cb02ed92080331e3c4
Joe Innes <joe@joeinn.es>
2017-10-19 10:43:41 +0200

    Made inject.js more readable. Closes #4

--- inject.js
+++ inject.js
@@ -3,11 +3,13 @@
 */
 chrome.extension.sendMessage({}, function (response) {
     var readyStateCheckInterval = setInterval(function () {
+        
+        // Poll to see if the document is ready
         if (document.readyState === 'complete') {
+            
+            // If so, then stop polling, and add the pull to reload functionality    
             clearInterval(readyStateCheckInterval);
-
             console.log('Mac Reload is running');
-
             AddDom();
         }
     }, 10);
@@ -15,12 +17,37 @@ chrome.extension.sendMessage({}, function (response) {
 
 function AddDom() {
     const elem = document.createElement('div');
-    elem.style = 'user-select: none; position: fixed; top: -20px; z-index: 99999; height: 15px; width: 100%; text-align: center;';
-    elem.id = 'p2r';
-    elem.innerText = 'Reloading...';
+    
+    const elemStyle = {
+        "userSelect": "none",
+        "position": "fixed",
+        "top": "20px",
+        "zIndex": 99999,
+        "height": "15px",
+        "width": "100%",
+        "textAlign": "center"
+    }
 
+    // Set element ID and message
+    elem.id = "p2r";
+    elem.innerText = "Reloading...";
+    
+    // Assign the styles to our element
+    Object.assign(elem.style, elemStyle)
+
+    // Insert our element into the body
     document.body.insertBefore(elem, document.body.firstChild);
 
     let isReloading = false;
-    overscroll(e => e.direction === 'up' && !isReloading && (console.log('reloading...'), isReloading = true) && document.location.reload());
+    overscroll((e) => {
+        
+        // If we're pulling to refresh, and the page isn't already refreshing
+        if (e.direction === 'up' && !isReloading) {
+            console.log('Reloading...');
+            isReloading = true;
+            
+            // Reload the page
+            document.location.reload();
+        }
+    });
 }
